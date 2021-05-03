63f4901e2e17bb50dbf11d98fa5c33df6a9bb0ea
aravind_jiny <aravind@jiny.io>
2021-05-03 17:29:02 +0530

    v0.0.2 release

--- Package.swift
+++ Package.swift
@@ -5,12 +5,20 @@ import PackageDescription
 let package = Package(
     name: "LeapCoreSDK",
     products: [
-        .library(name: "LeapCoreSDK", targets: ["LeapCoreSDK"]),
+        .library( name: "LeapCoreSDK", targets: ["LeapCoreSDKTargets"] )
     ],
     dependencies: [
         .package(name: "Gzip", url: "https://github.com/1024jp/GzipSwift.git", "5.1.1" ..< "5.2.0")
     ],
     targets: [
-        .binaryTarget(name: "LeapCoreSDK", path: "LeapCoreSDK.xcframework")
+        .binaryTarget(
+            name: "LeapCoreSDK", path: "LeapCoreSDK.xcframework"
+        ),
+        .target(name: "LeapCoreSDKTargets",
+                dependencies: [
+                    .target(name: "LeapCoreSDK"),
+                    .product(name: "Gzip", package: "Gzip")
+                ],
+                path: "Sources")
     ]
 )
